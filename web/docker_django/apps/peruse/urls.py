3197e1a18422f3eb4c817fc7c915caabcbf0aecc
David E. Oguche <Davazano@users.noreply.github.com>
2016-11-20 06:20:17 +0100

    Added details view for plants

--- web/docker_django/apps/peruse/urls.py
+++ web/docker_django/apps/peruse/urls.py
@@ -5,4 +5,5 @@ app_name = 'peruse'
 
 urlpatterns = [
     url(r'^$', views.index, name='peruse.index'),
+	url(r'^(?P<plant_id>[0-9]+)/$', views.plant_detail, name='peruse.plant_detail'),
 ]
--- web/docker_django/apps/peruse/views.py
+++ web/docker_django/apps/peruse/views.py
@@ -9,3 +9,9 @@ def index(request):
 	plants = Plant.objects.filter(is_visible = True)
 	counter = redis.incr('counter')
 	return render(request, 'library/index.html', {'plants': plants, 'counter': counter})
+
+def plant_detail(request, plant_id):
+	plant = get_object_or_404(Plant, pk = plant_id)
+	plant_images = PlantImage.objects.filter(plant = plant)
+	counter = redis.incr('counter')
+	return render(request, 'library/detail.html', {'plant': plant, 'plant_images': plant_images, 'counter': counter})
