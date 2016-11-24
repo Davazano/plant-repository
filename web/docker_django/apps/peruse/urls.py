cba05cc7ddc79aa2fe747672bd908aefc8a0f3fa
David E. Oguche <Davazano@users.noreply.github.com>
2016-11-24 15:13:48 +0100

    Added the python-requests and test route and view for testing OAR dataset submission

--- web/docker_django/apps/peruse/urls.py
+++ web/docker_django/apps/peruse/urls.py
@@ -6,4 +6,5 @@ app_name = 'peruse'
 urlpatterns = [
     url(r'^$', views.index, name='peruse.index'),
 	url(r'^(?P<plant_id>[0-9]+)/$', views.plant_detail, name='peruse.plant_detail'),
+	url(r'^test/', views.test, name='peruse.test')
 ]
--- web/docker_django/apps/peruse/views.py
+++ web/docker_django/apps/peruse/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404 # redirect
 from .models import Plant, PlantImage
 from redis import Redis
+from django.http import HttpResponse
+from django.contrib.staticfiles.storage import staticfiles_storage
 
+import requests
 
 redis = Redis(host='redis', port=6379)
 
@@ -15,3 +18,26 @@ def plant_detail(request, plant_id):
 	plant_images = PlantImage.objects.filter(plant = plant)
 	counter = redis.incr('counter')
 	return render(request, 'library/detail.html', {'plant': plant, 'plant_images': plant_images, 'counter': counter})
+
+def test(request):
+	# filePath = staticfiles_storage.url('library/xml/dataset-submission-to-OAR.xml')
+	filePath = '/usr/src/app/static/library/xml/dataset-submission-to-OAR.xml'
+	MIME = 'application/marcxml+xml'
+	OARurl = 'http://oar.sci-gaia.eu/batchuploader/robotupload/insert'
+	
+	headers = {
+		'Content-Type': 'application/marcxml+xml',
+		'User-Agent': 'invenio_webupload'
+	}
+
+	xmlfile = open(filePath, 'rb')
+	# data = xmlfile.readlines()
+
+	# r = requests.post(OARurl, data=data, headers=headers)
+	# r = requests.put(url, data=xmlfile, headers=headers, auth=HTTPDigestAuth("*", "*"))
+
+	files = {'file': (filePath, xmlfile, MIME)}
+	serverResponse = requests.put(OARurl, files=files, headers=headers)
+	response = HttpResponse(serverResponse)
+	# response = HttpResponse(filePath)
+	return  response
