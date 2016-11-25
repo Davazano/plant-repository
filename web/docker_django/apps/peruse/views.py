06edf4f3ce998a0d88c7f78fb524fdab6e2af4a6
David E. Oguche <Davazano@users.noreply.github.com>
2016-11-25 10:19:16 +0100

    Add auto generation of DOI

--- web/docker_django/apps/peruse/views.py
+++ web/docker_django/apps/peruse/views.py
@@ -22,8 +22,11 @@ def plant_detail(request, plant_id):
 	return render(request, 'library/detail.html', {'plant': plant, 'plant_images': plant_images, 'counter': counter})
 
 def test(request):
+	XMLPath = '/usr/src/app/static/library/xml/';
+	templateFile = XMLPath + 'temp/dataset-submission-to-OAR.xml'
+	testDir = 'test/'
+	XMLPath += testDir
 	counter = redis.incr('counter')
-	filePath = '/usr/src/app/static/library/xml/dataset-submission-to-OAR.xml'
 	MIME = 'application/marcxml+xml'
 	OARurl = 'http://oar.sci-gaia.eu/batchuploader/robotupload/insert'
 	
@@ -35,15 +38,67 @@ def test(request):
 	# now = datetime.datetime.now().strftime('%H:%M:%S')
 	seconds = datetime.datetime.now().strftime('%S')
 	now = time.mktime(datetime.datetime.now().timetuple())
-	doi = str(now).replace(".0", "") + '.' + seconds
+	doi = str(now).replace('.0', '') + '.' + seconds
 
-	file = open("/usr/src/app/static/library/dois.xml", "w")
-	file.write(str(counter) + "\t--\t" + doi  + "\n")
+	date = datetime.datetime.now().strftime('%Y-%M-%d')
+	author1 = 'Oguche David'
+	university1 = 'University of Jos'
+	country1 = 'Nigeria'
+	number1 = '0000-0002-5532-8201'
+	license = 'cc-by-nc-sa'
+	licenseURL = 'https://creativecommons.org/licenses/by-nc-sa/3.0/'
+	project = 'ACEPRD Plant Repository'
+	author2 = 'Ohaeri Uchechukwu'
+	university2 = 'University of Jos'
+	country2 = 'Nigeria'
+	number2 = '0000-0002-5532-8201'
+	tag1 = 'ACEPRD'
+	tag2 = 'Plant Repository'
+	tag3 = 'Open Access'
+	datasetFile = 'http://grid.ct.infn.it/hackfest/example-dataset.csv'
+
+	context = {
+		'doi' : doi,
+		'date' : date,
+		'author1' : author1, 
+		'university1' : university1,
+		'country1' : country1,
+		'number1' : number1,
+		'license' : license,
+		'licenseURL' : licenseURL,
+		'project' : project,
+		'author2' : author2,
+		'university2' : university2,
+		'country2' : country2,
+		'number2' : number2,
+		'tag1' : tag1,
+		'tag2' : tag2,
+		'tag3' : tag3,
+		'datasetFile' : datasetFile	
+	}
+
+
+	file = open(XMLPath + 'dois.txt', 'a')
+	file.write(str(counter) + '\t--\t' + doi  + '\n')
 	file.close()
 
-	xmlfile = open(filePath, 'rb')
-	files = {'file': (filePath, xmlfile, MIME)}
-	# serverResponse = requests.put(OARurl, files=files, headers=headers)
-	response = HttpResponse(doi)
-	
+
+	XMLTemplateFile = open(templateFile, 'r')
+	XMLStream = XMLTemplateFile.readlines()
+	templateStr = ''.join(XMLStream)
+	newXMLContentStr = templateStr.format(**context)
+	XMLTemplateFile.close()
+
+	XMLFile = XMLPath + doi  + '.xml';
+
+	file = open(XMLFile, 'w+')
+	file.write(newXMLContentStr)
+
+	files = {'file': (XMLFile, file, MIME)}
+	serverResponse = requests.put(OARurl, files=files, headers=headers)
+	resp = str(newXMLContentStr) + '<br><br>Server Response' + str(serverResponse)
+	response = HttpResponse(resp)
+
+	file.close()
+
 	return  response
